22aa2ed3e4ebf2200a35020b41912d60a97f2ce0
Say-Full <saifullah.sani01@gmail.com>
2023-11-14 09:12:36 +0700

    Menentukan pemenang

--- 3-reactjs/1-tic-tac-toe/src/App.jsx
+++ 3-reactjs/1-tic-tac-toe/src/App.jsx
@@ -11,7 +11,7 @@ export default function Board() {
 
   function handleClick(i) {
     
-    if( squares[i] ) return;
+    if( squares[i] || calculateWinner(squares) ) return;
 
     const newSquares = squares.slice();
 
@@ -20,17 +20,65 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status = '';
+
+  if( winner ) {
+    status = 'Pemenang: ' + winner;
+  } else {
+    status = 'Giliran: ' + (xIsNext ? 'X' : 'O') 
+  }
+
   return (
-    <div className='board'>
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)}  />
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)}  />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)}  />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)}  />
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)}  />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)}  />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)}  />
-    </div>
+    <>
+      <div className='status'>{ status }</div>
+      
+      <div className='board'>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}  />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}  />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}  />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}  />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}  />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}  />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}  />
+      </div>
+    </>
   )
+}
+
+
+
+// Logika penentuan pemenang
+function calculateWinner(squares) {
+  const lines = [
+    // Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    
+    // Vertikal
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i = 0; i < lines.length; i++) {
+    // const [a = lines[i][0];
+    // const b = lines[i][1];
+    // const c = lines[i][2];
+
+    const [a, b, c] = lines[i];
+
+    if( squares[a] && squares[a] === squares[b] && squares[a] === squares[c] ) {
+      return squares[a];
+    }
+
+    return false;
+  }
 }
