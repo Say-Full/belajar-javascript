ef7fd4669990bd676de1a05e826c2ac38d8d19d2
Say-Full <saifullah.sani01@gmail.com>
2023-11-17 08:51:16 +0700

    Memecah UI AnimeList

--- 3-reactjs/3-anime-app/src/App.jsx
+++ 3-reactjs/3-anime-app/src/App.jsx
@@ -110,24 +110,36 @@ function ListBox({ animes, onSelectedAnime }) {
     return(
         <div className="box">
             <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
-                {isOpen1 ? '–' : '+'}
+                {isOpen1 ? '-' : '+'}
             </button>
             {isOpen1 && (
-                <ul className="list list-anime">
-                    {animes?.map((anime) => (
-                        <li key={anime.mal_id} onClick={() => onSelectedAnimeonSelectedAnime(anime.mal_id)}>
-                            <img src={anime.image} alt={`${anime.title} cover`} />
-                            <h3>{anime.title}</h3>
-                            <div>
-                                <p>
-                                <span>{anime.year}</span>
-                                </p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                <AnimeList animes={animes} onSelectedAnime={onSelectedAnime} />
             )}
         </div>
+    );
+}
+
+function AnimeList({ animes, onSelectedAnime }) {
+    return(
+        <ul className="list list-anime">
+            {animes?.map((anime) => (
+                <Anime key={anime.mal_id} anime={anime} onSelectedAnime={onSelectedAnime} />
+            ))}
+        </ul>
+    );
+}
+
+function Anime({ anime, onSelectedAnime }) {
+    return(
+        <li onClick={() => onSelectedAnime(anime.mal_id)}>
+            <img src={anime.image} alt={`${anime.title} cover`} />
+            <h3>{anime.title}</h3>
+            <div>
+                <p>
+                <span>{anime.year}</span>
+                </p>
+            </div>
+        </li>
     );
 }
 
