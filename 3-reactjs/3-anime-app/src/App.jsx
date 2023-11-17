fce5dd7aecf88fab085f8244cde726e7b5db4bd4
Say-Full <saifullah.sani01@gmail.com>
2023-11-17 08:53:05 +0700

    Memecah UI AnimeDetail

--- 3-reactjs/3-anime-app/src/App.jsx
+++ 3-reactjs/3-anime-app/src/App.jsx
@@ -151,24 +151,28 @@ function SelectedBox({ selectedAnime }) {
             <button className="btn-toggle" onClick={() => setIsOpen2((open) => !open)}>
                 {isOpen2 ? '–' : '+'}
             </button>
-            {isOpen2 && (
-                <div className="details">
-                    <header>
-                        <img src={selectedAnime.image} alt={`${selectedAnime.title} cover`} />
-                        <div className="details-overview">
-                            <h2>{selectedAnime.title}</h2>
-                            <p>
-                                {selectedAnime.year} &bull; {selectedAnime.score}
-                            </p>
-                        </div>
-                    </header>
-                    <section>
-                        <p>
-                        <em>{selectedAnime.synopsis}</em>
-                        </p>
-                    </section>
+            {isOpen2 && <AnimeDetail selectedAnime={selectedAnime} />}
+        </div>
+    );
+}
+
+function AnimeDetail({ selectedAnime }) {
+    return(
+        <div className="details">
+            <header>
+                <img src={selectedAnime.image} alt={`${selectedAnime.title} cover`} />
+                <div className="details-overview">
+                    <h2>{selectedAnime.title}</h2>
+                    <p>
+                        {selectedAnime.year} &bull; {selectedAnime.score}
+                    </p>
                 </div>
-            )}
+            </header>
+            <section>
+                <p>
+                <em>{selectedAnime.synopsis}</em>
+                </p>
+            </section>
         </div>
     );
 }
