71341670c26b78e980e6bfd4410a2dad90f371a4
Say-Full <saifullah.sani01@gmail.com>
2023-11-19 00:38:24 +0700

    Membuat Navbar React-Bootstrap

---
+++ 3-reactjs/react-boostrap-crud/src/crud/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NavbarComponent from './NavbarComponent';
+
+export default class Crud extends Component {
+    render() {
+        return(
+            <div>
+                <NavbarComponent />
+                <h1>CRUD React-Bootstrap</h1>
+            </div>
+        );
+    }
+}
