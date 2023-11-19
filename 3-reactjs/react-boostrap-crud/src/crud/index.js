88aac7461c8a30ab449901a14669a4f04473f910
Say-Full <saifullah.sani01@gmail.com>
2023-11-19 20:50:58 +0700

    Membuat fitur Create

--- 3-reactjs/react-boostrap-crud/src/crud/index.js
+++ 3-reactjs/react-boostrap-crud/src/crud/index.js
@@ -4,15 +4,49 @@ import Tabel from './Tabel';
 import Formulir from './Formulir';
 
 export default class Crud extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            foods: [],
+            nama: "",
+            deskripsi: "",
+            harga: 0,
+            id: ""
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault(); // Menghilangkan reload halaman, yaitu fungsi asli suatu form, krn kita ingin membuat single-page application
+        
+        this.setState({
+            foods: [
+                ...this.state.foods,
+                {
+                    id: this.state.foods.length + 1,
+                    nama: this.state.nama,
+                    deskripsi: this.state.deskripsi,
+                    harga: this.state.harga
+                }
+            ]
+        });
+    };
+
     render() {
         return(
             <div>
                 <NavbarComponent />
                 <h1>CRUD React-Bootstrap</h1>
 
-                <div class="container mt-4">
+                <div className="container mt-4">
                     <Tabel />
-                    <Formulir />
+                    <Formulir {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
                 </div>
             </div>
         );
