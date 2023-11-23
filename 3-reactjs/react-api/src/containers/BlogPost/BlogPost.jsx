2cc36d041de89463d214104032d633374bec18fb
Say-Full <saifullah.sani01@gmail.com>
2023-11-23 10:53:50 +0700

    Menggunakan method fetch untuk mengambil data dari dummy API

--- 3-reactjs/react-api/src/containers/BlogPost/BlogPost.jsx
+++ 3-reactjs/react-api/src/containers/BlogPost/BlogPost.jsx
@@ -3,11 +3,28 @@ import './BlogPost.css';
 import Post from '../../components/Post/Post';
 
 class BlogPost extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            // .then(json => console.log(json)); // Berisi 100 array of object yg tiap object memiliki properti userId, id, tittle, dan body
+            .then(json => {
+                this.setState({
+                    posts: json
+                });
+            });
+    }
+    
     render() {
         return(
             <Fragment>
                 <p className='section-tittle'>Isi</p>
-                <Post tittle="Contoh tittle" desc="Contoh desc" />
+                { this.state.posts.map(post => {
+                    return <Post key={ post.id } tittle={ post.tittle } desc={ post.body } />
+                }) }
             </Fragment>
         );
     }
