0da33ddd97e5eb0d51f0fb68c170d6c619de05a8
Say-Full <saifullah.sani01@gmail.com>
2023-11-23 17:34:12 +0700

    Method POST

--- 3-reactjs/react-api/src/containers/BlogPost/BlogPost.jsx
+++ 3-reactjs/react-api/src/containers/BlogPost/BlogPost.jsx
@@ -5,15 +5,30 @@ import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
-        posts: []
+        posts: [],
+        formBlogPost: {
+            id: 1,
+            userId: 1,
+            title: '',
+            body: ''
+        }
     }
 
     fetchApiData = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
             .then(result => {
                 this.setState({
                     posts: result.data
                 });
+            });
+    }
+
+    postDataToApi = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost)
+            .then((res) => {
+                this.fetchApiData();
+            }, (err) => {
+                console.log(err);
             });
     }
 
@@ -24,11 +39,42 @@ class BlogPost extends Component {
     handleHapusPost = (id) => {
         axios.delete(`http://localhost:3004/posts/${id}`).then(res => { this.fetchApiData(); });
     }
+
+    handleFormChange = (event) => {
+        let newFormBlogPost = {...this.state.formBlogPost};
+        newFormBlogPost['id'] = new Date().getTime();
+        newFormBlogPost[event.target.name] = event.target.value;
+
+        this.setState({
+            formBlogPost: newFormBlogPost
+        }, () => {
+
+        });
+    }
+
+    handleSubmit = () => {
+        this.postDataToApi();
+    }
     
     render() {
         return(
             <Fragment>
                 <p className='section-tittle'>Isi</p>
+
+
+
+                <div className="form-tambah-post">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name='title' placeholder='Tambah judul' onChange={ this.handleFormChange } />
+
+                    <label htmlFor="body">Deskripsi</label>
+                    <textarea name="body" id="body" cols="30" rows="10" onChange={ this.handleFormChange } ></textarea>
+
+                    <button className='btn-submit' onClick={ this.handleSubmit }>Simpan</button>
+                </div>
+
+
+
                 { this.state.posts.map(post => {
                     return <Post key={ post.id } data={ post } hapusPost={ this.handleHapusPost } />
                 }) }
